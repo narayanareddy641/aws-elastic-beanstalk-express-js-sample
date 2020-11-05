034e104b1a31a5f46b363b5e6feb1bf272829f91
user <user@Apples-MacBook-Pro.local>
2020-11-05 21:52:08 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! to reddy123'));
+app.get('/', (req, res) => res.send('welcome to reddy's tech'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
