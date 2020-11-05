1c76eaa8e09b9e1f58793f97e91a87670c1407a2
user <user@Apples-MacBook-Pro.local>
2020-11-05 22:00:23 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('welcome to reddy's tech'));
+app.get('/', (req, res) => res.send('welcome to nmreddy's tech'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
